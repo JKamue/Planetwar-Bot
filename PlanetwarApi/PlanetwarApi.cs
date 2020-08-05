64f354805079d41119305695ab417a69622b6a4e
JKamue <mail@jkamue.de>
2020-08-05 14:10:43 +0200

    Added generic function for json decode and error detection

--- PlanetwarApi/PlanetwarApi.cs
+++ PlanetwarApi/PlanetwarApi.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using PlanetwarApi.Data;
 using PlanetwarApi.Objects;
+using PlanetwarApi.Objects.Information;
+using PlanetwarApi.Objects.Responses;
 
 namespace PlanetwarApi
 {
@@ -22,14 +25,24 @@ namespace PlanetwarApi
             string loginData = JsonConvert.SerializeObject(login);
             var response = _dataReceiver.SendRequest("login", new Dictionary<string, string>(), loginData);
 
-            if (response.StatusCode != HttpStatusCode.OK)
+            _accountKey = Decode<string>(response);
+            Console.WriteLine(_accountKey);
+        }
+
+        private T Decode<T>(Response response)
+        {
+            if (response.StatusCode == HttpStatusCode.OK)
             {
-                var error = JsonConvert.DeserializeObject<Error>(response.Data);
-                throw new Exception(error.description);
+                return JsonConvert.DeserializeObject<T>(response.Data);
+            }
+            
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new HttpRequestException("Endpoint not found");
             }
 
-            _accountKey = JsonConvert.DeserializeObject<string>(response.Data);
-            Console.WriteLine(_accountKey);
+            var error = JsonConvert.DeserializeObject<Error>(response.Data);
+            throw new Exception(error.description);
         }
     }
 }
